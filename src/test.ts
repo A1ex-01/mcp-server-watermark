8e1df9f164b5cec29a6b2f33ac023eab4a01acd7
a1ex <peterxhhx@gmail.com>
2025-05-30 17:12:37 +0800

    fix: ...

---
+++ src/test.ts
@@ -0,0 +1,9 @@
+import fs from "fs";
+import path from "path";
+
+const inputPathResolved = path.join(
+  "/Users/a1ex/Desktop/resources/pdfs/",
+  "A.pdf"
+);
+console.log(fs.existsSync(inputPathResolved));
+console.log(fs.existsSync(inputPathResolved));
